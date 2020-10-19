f2f3c29bdf0273ac39e8629653f754c99a89a34f
keno80 <1211582428@qq.com>
2020-10-19 17:55:40 +0800

    添加后台登录接口，添加jwt

--- router/user.js
+++ router/user.js
@@ -1,15 +1,47 @@
 const router = require('koa-router')()
+const jwt = require('jsonwebtoken')
 const dataQuery = require('../utils/query')
 const user_sql = require('../utils/user_sql')
 
+//用户登录
+router.post('/login', async (ctx, next) => {
+  let body = ctx.request.body
+
+  const userInfo = await dataQuery.query(user_sql.userSQL.USER_LOGIN(body.username))
+
+  if (userInfo.response.length !== 0) {
+    if (userInfo.response[0].password === body.password) {
+      const userToken = {
+        username: userInfo.response[0].username,
+        uid: userInfo.response[0].uid
+      }
+      const secret = 'node-koa-jwt'
+      const token = jwt.sign(userToken, secret)
+      ctx.response.body = {
+        code: 200,
+        message: '登录成功',
+        token
+      }
+    } else {
+      ctx.response.body = {
+        code: 201,
+        message: '登录失败，密码错误'
+      }
+    }
+  }
+  //
+  // console.log(`新请求 -> /userInfo, 请求参数 -> ${query.name}`);
+  // console.log(data);
+  //
+  // ctx.response.body = data
+})
+
 //获取用户信息
 router.get('/getInfo', async (ctx, next) => {
-  let query = ctx.request.query
-  // const data = await findUser(query.name)
-  const data = await dataQuery.query(user_sql.userSQL.QUERY_TABLE('user'))
 
-  console.log(`新请求 -> /userInfo, 请求参数 -> ${query.name}`);
-  console.log(data);
+  let data = await dataQuery.query(user_sql.userSQL.QUERY_TABLE('user'))
+
+  data.response[0].password = undefined
 
   ctx.response.body = data
 })
--- utils/user_sql.js
+++ utils/user_sql.js
@@ -10,21 +10,12 @@
 //查询数据库
 const QUERY_TABLE = (tableName) => `SELECT * FROM ${tableName};`
 
-//插入数据
-const INSERT_TABLE = (tableName, {key, val}) => `INSERT INTO ${tableName}(${key}) VALUES (${val});`
-
-//更新数据
-const UPDATE_TABLE = (tableName, {primaryKey, primaryVal}, {key, val}) => `UPDATE ${tableName} SET ${key}=${val} WHERE(${primaryKey}=${primaryVal});`
-
-//删除数据
-const DELETE_TABLE = (tableName, {primaryKey, primaryVal}) => `DELETE FROM user WHERE(${primaryKey}=${primaryVal})`
+const USER_LOGIN = (username) => `SELECT * FROM user WHERE username='${username}';`
 
 const userSQL = {
   CREATE_TABLE,
   QUERY_TABLE,
-  INSERT_TABLE,
-  UPDATE_TABLE,
-  DELETE_TABLE
+  USER_LOGIN
 }
 
 module.exports = {
