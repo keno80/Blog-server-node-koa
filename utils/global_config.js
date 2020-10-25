0a08a436bf6d487e939ed5e9184191b93b3a52cf
keno80 <1211582428@qq.com>
2020-10-25 22:31:31 +0800

    Create global_config.js

---
+++ utils/global_config.js
@@ -0,0 +1,14 @@
+const config = {
+  port: 9090,
+
+  database: {
+    database: 'blog',
+    user: 'root',
+    password: 'password',
+    port: '3306',
+    host: 'localhost',
+    multipleStatements: true
+  }
+}
+
+module.exports = config
